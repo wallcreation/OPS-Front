e2b425821b221251bbe5701b1819a1716cbcd994
wallcreation <lanhajames@gmail.com>
2025-06-02 16:37:44 +0100

    feat: add AdminHeader component with layout and icons

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -3,9 +3,13 @@ import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import api from '@/api'
 import { onMounted } from 'vue'
+import AdminHeader from '@/components/AdminHeader.vue'
 onMounted(() => {})
 </script>
 
 <template>
-  <p>dashboard</p>
+  <AdminHeader />
+  <main class="bg-[url('/generalbg.jpg')] bg-cover h-screen w-full">
+    <p>dashboard</p>
+  </main>
 </template>

20b45c8466acf4e3cbc3fd7257f135a58ecee682
wallcreation <lanhajames@gmail.com>
2025-06-15 22:56:40 +0100

    Remove uneccesary file and setupp new structure

--- src/router/index.js
+++ src/router/index.js
@@ -5,27 +5,19 @@ import AdminView from '@/views/AdminView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      redirect: '/login',
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView,
-    },
+    // {
+    //   path: '/',
+    //   redirect: '/login',
+    // },
+    // {
+    //   path: '/login',
+    //   name: 'login',
+    //   component: LoginView,
+    // },
     {
       path: '/admin/dashboard',
       name: 'Dashboard',
       component: AdminView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
     },
   ],
 })
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,63 +1,6 @@
 <script setup>
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-import api from '@/api'
-import { onMounted } from 'vue'
-import AccountListV from '@/components/AccountListV.vue'
-import AdminHeader from '@/components/AdminHeader.vue'
-import OperatorListV from '@/components/OperatorListV.vue'
-import TeamListH from '@/components/TeamListH.vue'
-const router = useRouter()
-const me = ref({})
-const dashboard = ref({
-  teams: [],
-  operators: [],
-  accounts: [],
-})
-onMounted(() => {
-  const token = localStorage.getItem('token')
-  if (!token) {
-    router.push('/login')
-    return
-  }
-  api
-    .get('/me')
-    .then((response) => {
-      me.value = response.data.user
-    })
-    .catch((error) => {
-      console.error('Error fetching admin data:', error)
-      router.push('/login')
-    })
-
-  api
-    .get('/admin/dashboard')
-    .then((response) => {
-      dashboard.value = response.data
-      console.log('Dashboard data:', dashboard)
-    })
-    .catch((error) => {
-      console.error('Error fetching dashboard data:', error)
-    })
-})
+import AdminSide from '@/components/AdminSide.vue'
 </script>
-
 <template>
-  <AdminHeader
-    :fname="me.fname"
-    :lname="me.lname"
-    :teams="dashboard.teams.length"
-    :operators="dashboard.operators.length"
-    :accounts="dashboard.accounts.length"
-    class=""
-  />
-  <main class="h-screen">
-    <div class="p-2">
-      <TeamListH :teams="dashboard.teams" />
-    </div>
-    <div class="flex justify-center gap-2 p-2">
-      <OperatorListV :operators="dashboard.operators" class="w-1/2" />
-      <AccountListV :accounts="dashboard.accounts" class="w-1/2" />
-    </div>
-  </main>
+    <AdminSide/>
 </template>

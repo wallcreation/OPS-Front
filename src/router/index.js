a00296c3743eb8a4a1b85ce8a33d7d5a5e0c5868
wallcreation <lanhajames@gmail.com>
2025-06-16 10:05:00 +0100

    refactor: comment out unused LoginView import in router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '@/views/LoginView.vue'
+// import LoginView from '@/views/LoginView.vue'
 import AdminView from '@/views/AdminView.vue'
 
 const router = createRouter({

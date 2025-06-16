fe799a6b620f70091db50f0b7d87d2d6e328d55a
wallcreation <lanhajames@gmail.com>
2025-06-16 22:04:57 +0100

    feat: restructure Admin components and update AdminView with AdminDashboard

--- src/components/AdminSide.vue
+++ src/components/AdminSide.vue
@@ -1,6 +1,14 @@
 <script setup></script>
 <template>
-    <div class="">
-    	<a>Acceuil</a>
+  <aside class="bg-gray-800 w-1/4 h-screen fixed top-0 left-0">
+    <div>
+      <a href="#" class="block text-white">Acceuil</a>
+      <a href="#" class="block text-white">Equipes</a>
+      <a href="#" class="block text-white">Opérateurs</a>
+      <a href="#" class="block text-white">Comptes</a>
     </div>
+    <div>
+        
+    </div>
+  </aside>
 </template>
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,6 +1,9 @@
 <script setup>
 import AdminSide from '@/components/AdminSide.vue'
+import AdminDashboard from '@/components/AdminDashboard.vue';
 </script>
 <template>
+    <h1>jgjg</h1>
     <AdminSide/>
+    <AdminDashboard/>
 </template>

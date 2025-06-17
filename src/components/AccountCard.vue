53a3649ffa975aed9767e1fd42bae44dc7ef5e6b
wallcreation <lanhajames@gmail.com>
2025-06-17 05:15:45 +0000

    Add icon and responsive to adminside

---
+++ src/components/AccountCard.vue
@@ -0,0 +1,2 @@
+<script setup>
+<scipt>
--- src/components/AdminSide.vue
+++ src/components/AdminSide.vue
@@ -3,41 +3,111 @@
     <div>
       <a
         href="#"
-        class="block my-1 p-1 rounded-lg border-3 border-[#333333] hover:border-[#3B91F9] hover:text-[#3B91F9]"
-        >Acceuil</a
+        class="flex items-center gap-2 my-1 p-1 rounded-lg border-3 border-[#333333] hover:border-[#3B91F9] hover:text-[#3B91F9]"
       >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+          stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 
+            .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 
+            1.125-1.125h2.25c.621 0 1.125.504 
+            1.125 1.125V21h4.125c.621 0 1.125-.504 
+            1.125-1.125V9.75M8.25 21h8.25" />
+        </svg>
+        <span class="hidden md:inline">Accueil</span>
+      </a>
+
       <a
         href="#"
-        class="block my-1 p-1 rounded-lg border-3 border-[#333333] hover:border-[#3B91F9] hover:text-[#3B91F9]"
-        >Equipes</a
+        class="flex items-center gap-2 my-1 p-1 rounded-lg border-3 border-[#333333] hover:border-[#3B91F9] hover:text-[#3B91F9]"
       >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+          stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 
+            3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 
+            .225-.012.447-.037.666A11.944 11.944 0 0 1 
+            12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 
+            6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 
+            0-.941-3.197m0 0A5.995 5.995 0 0 0 12 
+            12.75a5.995 5.995 0 0 0-5.058 
+            2.772m0 0a3 3 0 0 0-4.681 2.72 
+            8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 
+            5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 
+            1-6 0 3 3 0 0 1 6 0Z" />
+        </svg>
+        <span class="hidden md:inline">Équipes</span>
+      </a>
+
       <a
         href="#"
-        class="block my-1 p-1 rounded-lg border-3 border-[#333333] hover:border-[#3B91F9] hover:text-[#3B91F9]"
-        >Opérateurs</a
+        class="flex items-center gap-2 my-1 p-1 rounded-lg border-3 border-[#333333] hover:border-[#3B91F9] hover:text-[#3B91F9]"
       >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+          stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 
+            3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 
+            1 14.998 0A17.933 17.933 0 0 1 12 
+            21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+        </svg>
+        <span class="hidden md:inline">Opérateurs</span>
+      </a>
+
       <a
         href="#"
-        class="block my-1 p-1 rounded-lg border-3 border-[#333333] hover:border-[#3B91F9] hover:text-[#3B91F9]"
-        >Comptes</a
+        class="flex items-center gap-2 my-1 p-1 rounded-lg border-3 border-[#333333] hover:border-[#3B91F9] hover:text-[#3B91F9]"
       >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+          stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 
+            2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 
+            0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 
+            48.114 0 0 0-3.413-.387m4.5 
+            8.006c-.194.165-.42.295-.673.38A23.978 
+            23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 
+            2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 
+            3 12.489V8.706c0-1.081.768-2.015 
+            1.837-2.175a48.111 48.111 0 0 1 
+            3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 
+            13.5 3h-3a2.25 2.25 0 0 0-2.25 
+            2.25v.894m7.5 0a48.667 48.667 0 0 
+            0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+        </svg>
+        <span class="hidden md:inline">Comptes</span>
+      </a>
     </div>
-    <div class="flex items-center gap-2 items-center justify-center">
-      <a href="#" class="w-3/4 p-1 rounded-lg border-3 border-[#333333] hover:border-[#EB5757] hover:text-[#EB5757]">Admin</a>
-      <a href="#" class="p-1 rounded-lg border-3 border-[#333333] hover:border-[#3B91F9] hover:text-[#3B91F9]">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="size-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-          />
+
+    <div class="mx-2 flex items-center gap-1 justify-center">
+      <a
+        href="#"
+        class="flex items-center gap-2 w-3/4 p-1 rounded-lg border-3 border-[#333333] hover:border-[#EB5757] hover:text-[#EB5757]"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+          stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 
+            3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 
+            2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 
+            1-2.25-2.25V15m-3 0-3-3m0 0 
+            3-3m-3 3H15" />
+        </svg>
+        <span class="hidden md:inline">Admin</span>
+      </a>
+
+      <a
+        href="#"
+        class="p-1 rounded-lg border-3 border-[#333333] hover:border-[#3B91F9] hover:text-[#3B91F9]"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+          stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M16.023 9.348h4.992v-.001M2.985 
+            19.644v-4.992m0 0h4.992m-4.993 0 
+            3.181 3.183a8.25 8.25 0 0 0 
+            13.803-3.7M4.031 9.865a8.25 8.25 0 0 
+            1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
         </svg>
       </a>
     </div>

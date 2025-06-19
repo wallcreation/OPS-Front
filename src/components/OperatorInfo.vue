eb8d47ceb8930befd3879470e23d0126cb7790f7
wallcreation <lanhajames@gmail.com>
2025-06-19 13:55:46 +0100

    refactor: simplify date formatting and enhance stat display in OperatorInfo component

--- src/components/OperatorInfo.vue
+++ src/components/OperatorInfo.vue
@@ -19,10 +19,7 @@ function getpenaltyamount(date) {
 function getpenaltyraison(date) {
   return props.penalty.find((p) => p.date === date)?.reason
 }
-// function formatDate(dateStr, options = { weekday: 'short', day: '2-digit' }) {
-//   const date = new Date(dateStr)
-//   return new Intl.DateTimeFormat('fr-FR', options).format(date)
-// }
+
 function formatDate(dateStr) {
   const date = new Date(dateStr)
   return new Intl.DateTimeFormat('fr-FR', {
@@ -102,7 +99,6 @@ function getPenaltyReason(date) {
     </div>
     <!--Stat table-->
     <div class="mt-2">
-      <div></div>
       <table
         class="w-full table-auto border border-[#2F80ED] text-sm text-white rounded-lg overflow-hidden"
       >
@@ -126,12 +122,12 @@ function getPenaltyReason(date) {
             <td class="px-3 py-2">
               {{ formatDate(stat.date) }}
             </td>
-            <td class="px-3 py-2 text-[#2F80ED]">
+            <td class="px-3 py-2 text-[#2F80ED] flex items-center">
               <div>
                 <p class="inline me-5">
                   {{ stat.total }} <sup class="text-xs text-gray-400">{{ stat.stop_total }}</sup>
                 </p>
-                <p class="inline text-xs text-gray-400">
+                <p class="hidden md:inline text-xs text-gray-400">
                   {{ stat.start }} <sup>{{ stat.stop_start }}</sup>
                   <span
                     ><svg
@@ -140,7 +136,7 @@ function getPenaltyReason(date) {
                       viewBox="0 0 24 24"
                       stroke-width="1.5"
                       stroke="currentColor"
-                      class="size-6 inline"
+                      class="size-4 inline text-[#2F80ED]"
                     >
                       <path
                         stroke-linecap="round"
@@ -153,6 +149,22 @@ function getPenaltyReason(date) {
                   <sup>{{ stat.stop_end }}</sup>
                 </p>
               </div>
+              <button class="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-4 text-[#EB5757]"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </button>
             </td>
             <td class="px-3 py-2 text-[#EB5757]" :title="getPenaltyReason(stat.date)">
               {{ getPenaltyAmount(stat.date) }} - {{ getPenaltyReason(stat.date) }}

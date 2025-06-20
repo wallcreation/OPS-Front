8a3e9f869eb3afab6f4164b3fb012e5cace7cbab
wallcreation <lanhajames@gmail.com>
2025-06-20 14:13:03 +0100

    refactor: standardize import statement and improve code formatting in routes.js

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,1 +1,90 @@
-import api from "./base"
+import api from './base'
+
+async function login(email, password) {
+  try {
+    const response = await api.post('/login', { email, password })
+    if (response.data.status && response.status === 200) {
+      localStorage.setItem('token', response.data.token)
+      return [true, response.data.profile]
+    }
+  } catch (error) {
+    return [false]
+  }
+}
+
+async function team_by_id(id) {
+  try {
+    const response = await api.get(`/team/${id}`)
+    if (response.data.status && response.status === 200) {
+      return [true, response.data.team]
+    }
+  } catch (error) {
+    return [false]
+  }
+}
+
+async function team_by_name(name) {
+  try {
+    const response = await api.get(`/team_name/${name}`)
+    if (response.data.status && response.status === 200) {
+      return [true, response.data.team]
+    }
+  } catch (error) {
+    return [false]
+  }
+}
+
+async function teams() {
+  try {
+    const response = await api.get('/teams')
+    if (response.data.status && response.status === 200) {
+      return [true, response.data.teams]
+    }
+  } catch (error) {
+    return [false]
+  }
+}
+
+async function teams_name() {
+  try {
+    const response = await api.get('/teams_name')
+    if (response.data.status && response.status === 200) {
+      return [true, response.data.teams]
+    }
+  } catch (error) {
+    return [false]
+  }
+}
+
+async function create_team(name) {
+    try {
+        const response = await api.post('/team', { name })
+        if (response.data.status && response.status === 200) {
+        return [true, response.data.team]
+        }
+    } catch (error) {
+        return [false]
+    }
+}
+
+async function delete_team(id) {
+  try {
+    const response = await api.delete(`/team/${id}`)
+    if (response.data.status && response.status === 200) {
+      return [true]
+    }
+  } catch (error) {
+    return [false]
+  } 
+}
+
+async function update_team(id, name) {
+  try {
+    const response = await api.put(`/team/${id}`, { name })
+    if (response.data.status && response.status === 200) {
+      return [true, response.data.team]
+    }
+  } catch (error) {
+    return [false]
+  }
+}

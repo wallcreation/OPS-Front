9d133fa5b4b9eca27e806a06f63b598003c6e746
wallcreation <lanhajames@gmail.com>
2025-06-27 20:00:55 +0100

    feat: refactor OperatorInfo and TeamInfo components for improved layout and structure; enhance readability and maintainability

--- src/views/OperatorInfo.vue
+++ src/views/OperatorInfo.vue
@@ -13,7 +13,20 @@ const operator = ref({
 })
 </script>
 <template>
-    <div class="grid grid-cols-1 md:grid-cols-4">
-        <h1>{{ fname }}</h1>
+    <div class="grid grid-cols-1 gap-2 md:grid-cols-4 mt-2">
+        <div class="md:col-span-2 p-2 bg-surface border-2 border-border rounded-lg">
+            <h1 class="text-5xl">{{ operator.fname }} {{ operator.lname }}</h1>
+            <div class="flex gap-2">
+                <p>{{ operator.email }}</p>
+                <p>{{ operator.team }}</p>
+                <button>Editer l'opérateur</button>
+            </div>
+        </div>
+        <div class="bg-surface p-2 border-2 border-border rounded-lg">
+            <h1>Statistiques</h1>
+        </div>
+        <div class="bg-surface p-2 border-2 border-border rounded-lg">
+            <h1>Pénalités</h1>
+        </div>
     </div>
 </template>
--- src/views/TeamInfo.vue
+++ src/views/TeamInfo.vue
@@ -15,76 +15,26 @@ const team = ref({
 const currentview = ref(1)
 </script>
 <template>
-  <div class="max-w-full max-h-full px-2">
-    <div class="w-full mt-2 grid gap-2 grid-cols-1 md:grid-cols-4 md:justify-between rounded-lg">
-      <div class="col-span-3 bg-surface rounded-lg border-2 border-border p-2">
-        <h1 class="text-5xl">{{ team.name }}</h1>
-        <button class="flex items-center hover:border-b-1 hover:border-primary-light">
-          <span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
-              <path
-                fill="currentColor"
-                d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1q-.15.15-.15.36M20.71 7.04a.996.996 0 0 0 0-1.41l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83l3.75 3.75z"
-              />
-            </svg>
-          </span>
-          <span class="">Editer l'équipe</span>
-        </button>
+  <section class="w-full mt-2 grid gap-2 grid-cols-1 md:grid-cols-4">
+    <section class="md:col-span-2 p-2 flex gap-2 justify-between bg-surface rounded-lg border-2 border-border">
+      <h1 class="text-5xl text-primary">{{ team.name }}</h1>
+      <button>Editer l'équipe</button>
+    </section>
+    <section class="p-2 bg-surface rounded-lg border-2 border-border">
+      <h2>Opérateurs</h2>
+      <div class="flex flex-wrap gap-1 ">
+        <p v-for="operator in team.operators" :key="operator">
+          {{ operator }}
+        </p>
       </div>
-      <div class="flex gap-2 bg-surface rounded-lg border-2 border-border p-2">
-        <div>
-          <h2 class="mb-1 font-bold">Operateurs</h2>
-          <ul class="flex gap-2">
-            <li v-for="operator in team.operators" :key="operator" class="mb-2 text-sm">
-              {{ operator }}
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h2 class="mb-1 font-bold">Comptes</h2>
-          <ul class="flex gap-2">
-            <li v-for="account in team.accounts" :key="account" class="mb-2 text-sm">
-              {{ account }}
-            </li>
-          </ul>
-        </div>
+    </section>
+    <section class="p-2 bg-surface rounded-lg border-2 border-border">
+      <h2>Comptes</h2>
+      <div class="flex flex-wrap gap-1 ">
+        <p v-for="account in team.accounts" :key="account">
+          {{ account }}
+        </p>
       </div>
-    </div>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-2 p-1 mt-2 outline-2 outline-border rounded-lg">
-      <div class="w-full bg-surface rounded-lg p-3">
-        <table class="w-full text-left">
-          <thead class="border-b-1 border-primary-light">
-            <tr>
-              <th>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  class="inline pb-1"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M12 12q-1.65 0-2.825-1.175T8 8t1.175-2.825T12 4t2.825 1.175T16 8t-1.175 2.825T12 12m-8 8v-2.8q0-.85.438-1.562T5.6 14.55q1.55-.775 3.15-1.162T12 13t3.25.388t3.15 1.162q.725.375 1.163 1.088T20 17.2V20z"
-                  />
-                </svg>
-              </th>
-              <th>Entrants</th>
-              <th>Pénalités</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="operator in team.operators" :key="operator" class="font-normal">
-              <th>{{ operator }}</th>
-              <th>50500</th>
-              <th>1024</th>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div class="w-full bg-surface rounded-lg p-5">
-        <h3></h3>
-      </div>
-    </div>
-  </div>
+    </section>
+  </section>
 </template>

da542d2035ae75c3cc17498983124fe5e209a6bd
wallcreation <lanhajames@gmail.com>
2025-07-05 09:00:32 +0000

    Fix AccountAdd.vue, add logic for add account and handle error

--- src/api/account.js
+++ src/api/account.js
@@ -1,5 +1,9 @@
 import api from "./base"
 
+export function createaccount(data) {
+    return api.post('/admin/account', data)
+}
+
 export function getaccounts() {
-    return api.get("/admin/accounts")
+    return api.get('/admin/accounts')
 }
--- src/components/admin/AccountAdd.vue
+++ src/components/admin/AccountAdd.vue
@@ -1,23 +1,42 @@
 <script setup>
 import { ref } from 'vue'
-import { safeCall } from '@/api'
+import { createaccount, safeCall } from '@/api'
 import TeamSelector from './TeamSelector.vue'
 const props = defineProps({
   showModal: Boolean,
   teams: Array,
 })
-const emit = defineEmits(['close'])
-const close = async () => {
-  emit('close')
-}
+const emit = defineEmits(['close', 'created'])
+// Variable
+const accountipt = ref('')
+const loading = ref(false)
+const error = ref([false, ''])
 const team = ref({ show: false, teamid: 0, textvalue: '', list: props.teams })
 //mntconst teamipt = ref('')
 
-const addteam = async () => {
-  console.log('Team name:')
+const addaccount = async () => {
+  loading.value = true
+  if (!accountipt.value) {
+    error.value = [true, 'Le nom du compte ne peut être vide']
+    setTimeout(() => {
+      error.value = [false, '']
+    }, 5000)
+    loading.value = false
+    return
+  }
+  const data = { name: accountipt.value, team_id: team.value.teamid }
+  const [res, err] = await safeCall(createaccount(data))
+  if (err) {
+    console.log('acc cre err: ', err)
+    error.value = [true, err.message]
+  } else {
+    console.log('acc cre res: ', res)
+    emit('created', res)
+  }
+  loading.value = false
 }
 // const teamsel = ref(false)
-const closetteamselector = async (name) => {
+const closetteamselector = async (team) => {
   team.value.show = false
   team.value.teamid = team.id
   team.value.textvalue = team.name
@@ -26,42 +45,52 @@ const closetteamselector = async (name) => {
 <template>
   <div
     v-if="showModal"
-    class="fixed inset-0 z-50 flex items-center justify-center bg-bg/50 backdrop-blur-md"
+    class="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-md"
   >
-    <div
-      class="mx-5 p-3 max-w-lg w-full rounded-lg border-1 border-border"
-      :class="[team.show ? 'bg-surface' : 'backdrop-blur-md']"
-    >
-      <h1 class="text-xl font-bold">Ajouter un compte</h1>
-      <form action="" class="my-2 grid gap-2 grid-cols-1 md:grid-cols-2">
+    <div class="mx-5 p-3 max-w-lg w-full rounded-lg border-1 border-border bg-surface">
+      <h1 class="text-xl text-center font-bold">Ajouter un compte</h1>
+      <form action="" class="my-2 grid gap-2 grid-cols-1 md:grid-cols-3">
         <input
           type="text"
           name="aname"
           id="aname"
           required
-          placeholder="Nom"
-          class="border-b-2 border-border focus:border-primary hover:border-primary-dark outline-none"
+          placeholder="Nom du compte"
+          :value="accountipt"
+          class="col-span-3 md:col-span-2 border-b-2 border-border focus:border-primary hover:border-primary-dark outline-none"
         />
         <input
           type="text"
           name="team"
           id="team"
-          placeholder="Equipe"
+          placeholder="Équipe"
           :value="team.textvalue"
-          class="border-b-2 border-border focus:border-primary hover:border-primary-dark outline-none"
+          class="col-span-3 md:col-span-1 border-b-2 border-border focus:border-primary hover:border-primary-dark outline-none"
           @click="team.show = true"
         />
       </form>
-      <div class="flex gap-2 justify-end mt-1">
-        <button @click="addteam" class="text-primary hover:border-b-2 hover:border-primary">
+      <div v-if="error[0]" class="w-full p-1 bg-error">
+        <p class="text-lg font-bold text-center">{{ error[1] }}</p>
+      </div>
+      <div class="flex gap-2 items-center justify-end mt-1">
+        <button
+          @click="addaccount"
+          :disabled="loading"
+          class="px-2 border-2 border-primary rounded-lg hover:border-primary-dark hover:bg-primary-dark"
+          :class="loading ? 'animate-pulse' : ''"
+        >
           Valider
         </button>
-        <button @click="close" class="text-error hover:border-b-2 hover:border-error">
+        <button
+          @click="emit('close')"
+          class="px-2 border-2 border-error rounded-lg hover:border-error-dark hover:bg-error-dark"
+        >
           Annuler
         </button>
       </div>
     </div>
   </div>
+  <!-- Quick team selector -->
   <TeamSelector
     :show="team.show"
     :teamlist="team.list"

b73306a503df3372e05b0fd7496e7ddab8e5de55
wallcreation <lanhajames@gmail.com>
2025-07-08 12:30:46 +0100

    feat: update API base URL to use production endpoint

--- src/api/base.js
+++ src/api/base.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://djetovi-api.onrender.com', // 'http://localhost:8000',
   headers: {
     'Content-Type': 'application/json',
   },

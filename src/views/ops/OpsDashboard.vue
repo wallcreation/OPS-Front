48f5868d3843c7fba7b5750a19c858d571ad4915
wallcreation <lanhajames@gmail.com>
2025-08-04 06:46:47 +0100

    Add icon to locked

--- src/views/ops/OpsDashboard.vue
+++ src/views/ops/OpsDashboard.vue
@@ -111,6 +111,22 @@ onMounted(async () => {
             </svg>
           </p>
           <p>{{ profile.code }}</p>
+          <p v-if="profile.is_locked" class="text-primary">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 256 256">
+              <g fill="currentColor">
+                <path d="M176 128a48 48 0 1 1-48-48a48 48 0 0 1 48 48" opacity="0.2" />
+                <path d="M140 128a12 12 0 1 1-12-12a12 12 0 0 1 12 12" />
+              </g>
+            </svg>
+          </p>
+          <p v-if="profile.is_locked" class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M6 7.5a5.5 5.5 0 1 1 11 0a5.5 5.5 0 0 1-11 0M18 14c.69 0 1.25.56 1.25 1.25V16h-2.5v-.75c0-.69.56-1.25 1.25-1.25m3.25 2v-.75a3.25 3.25 0 0 0-6.5 0V16h-1.251v6.5h9V16zm-9.75 6H2v-2a6 6 0 0 1 6-6h3.5z"
+              />
+            </svg>
+          </p>
         </div>
       </section>
       <div class="col-span-2 grid grid-cols-2 gap-1">
